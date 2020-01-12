523a27bca35cf35e8ab9b15d0c44f4d0f44dd25a
silentbaws <silentbaws@hotmail.com>
2020-01-12 05:02:47 -0500

    Add main and replay patch back

---
+++ XLMultiplayer/Main.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+using UnityModManagerNet;
+using Harmony12;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine.SceneManagement;
+
+namespace XLMultiplayer {
+	class Main {
+		public static bool enabled;
+		public static String modId;
+		public static UnityModManager.ModEntry modEntry;
+
+		public static HarmonyInstance harmonyInstance;
+
+		static void Load(UnityModManager.ModEntry modEntry) {
+			Main.modEntry = modEntry;
+			Main.modId = modEntry.Info.Id;
+
+			modEntry.OnToggle = OnToggle;
+		}
+
+		static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) {
+			if (value == enabled) return true;
+			enabled = value;
+
+			if (enabled) {
+				//Patch the replay editor
+				harmonyInstance = HarmonyInstance.Create(modEntry.Info.Id);
+				harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+
+			} else {
+				//Unpatch the replay editor
+				harmonyInstance.UnpatchAll(harmonyInstance.Id);
+
+			}
+
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(ReplayEditor.ReplayPlaybackController), "SetPlaybackTime")]
+	static class ReplaySetPlaybackTimePatch {
+
+		/* REPLACE ldc.r4 0 with
+			ldarg.0 NULL
+			call System.Collections.Generic.List`1[ReplayEditor.ReplayRecordedFrame] get_ClipFrames()
+			ldc.i4.0 NULL
+			callvirt ReplayEditor.ReplayRecordedFrame get_Item(Int32)
+			ldfld System.Single time
+		 */
+
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			foreach (CodeInstruction instruction in instructions) {
+				if (instruction.opcode == OpCodes.Ldc_R4) {
+					//BIG SHOUTOUT BLENDERMF
+					yield return new CodeInstruction(OpCodes.Ldarg_0);
+
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.Property(typeof(ReplayEditor.ReplayPlaybackController), "ClipFrames").GetMethod);
+
+					yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+
+					yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Property(typeof(List<ReplayEditor.ReplayRecordedFrame>), "Item").GetMethod);
+
+					yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ReplayEditor.ReplayRecordedFrame), "time"));
+				} else {
+					yield return instruction;
+				}
+			}
+		}
+	}
+}
